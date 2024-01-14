a9f6dd8fb30ee06e9253930fa56f3d0341b575f4
Rasmus Cederdorff <rasmus@cederdorff.com>
2024-01-14 21:51:30 +0100

    Create photos.$photoId_.update.jsx

---
+++ app/routes/photos.$photoId_.update.jsx
@@ -0,0 +1,30 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { mapFirebaseDocument } from "../helpers/firebaseDataMapper";
+
+export function meta({ data }) {
+    return [
+        {
+            title: `Remix Photo App - ${data.photo.caption || "Photo"}`
+        }
+    ];
+}
+
+export async function loader({ params }) {
+    const response = await fetch(
+        `https://firestore.googleapis.com/v1/projects/race-photo-app/databases/(default)/documents/photos/${params.photoId}`
+    );
+    const docs = await response.json();
+    const photo = mapFirebaseDocument(docs);
+    return json({ photo });
+}
+
+export default function UpdatePhoto() {
+    const { photo } = useLoaderData();
+    return (
+        <div className="page">
+            <h1>Update: {photo.caption}</h1>
+            <img src={photo.image} alt={photo.caption} />
+        </div>
+    );
+}
