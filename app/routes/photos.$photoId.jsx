18b45be6894a084f2cc35b8c212d502f777ef250
Rasmus Cederdorff <rasmus@cederdorff.com>
2024-01-14 17:30:23 +0100

    fetch one photo in loader

--- app/routes/photos.$photoId.jsx
+++ app/routes/photos.$photoId.jsx
@@ -1,22 +1,29 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { mapFirebaseDocument } from "../helpers/firebaseDataMapper";
 
 export const meta = () => {
     return [{ title: "Remix Photo App" }];
 };
 
 export async function loader({ params }) {
-    console.log(params);
-    return json({ photoId: params.photoId });
+    const response = await fetch(
+        `https://firestore.googleapis.com/v1/projects/race-photo-app/databases/(default)/documents/photos/${params.photoId}`
+    );
+    const docs = await response.json();
+    const photo = mapFirebaseDocument(docs);
+    return json({ photo });
 }
 
 export default function Photo() {
-    const { photoId } = useLoaderData();
-    console.log(photoId);
+    const { photo } = useLoaderData();
+    console.log(photo);
     return (
         <div className="page">
             <h1>Photo Placeholder page</h1>
-            <p>Photo id: {photoId}</p>
+            <p>Photo id: {photo.id}</p>
+            <p>Photo caption: {photo.caption}</p>
+            <img src={photo.image} alt={photo.caption} />
         </div>
     );
 }
