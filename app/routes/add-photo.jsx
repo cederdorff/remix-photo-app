bb52de4647418e8d33ada1a4874cd5b6cc78ce35
Rasmus Cederdorff <rasmus@cederdorff.com>
2024-01-15 20:21:53 +0100

    Update add-photo.jsx

--- app/routes/add-photo.jsx
+++ app/routes/add-photo.jsx
@@ -1,13 +1,56 @@
+import { redirect } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+import { useState } from "react";
+
 export const meta = () => {
     return [{ title: "Remix Photo App - Add New Photo" }];
 };
 export default function AddPhoto() {
+    const [image, setImage] = useState("");
+
     return (
         <div className="page">
             <h1>Add a Photo</h1>
-            <p>
-                This is a simple photo app built with <a href="https://remix.run">Remix</a>.
-            </p>
+            <Form id="contact-form" method="post">
+                <p>
+                    <span>Caption</span>
+                    <input name="caption" type="text" aria-label="caption" placeholder="Write a caption..." />
+                </p>
+                <label>
+                    <span>Image URL</span>
+                    <input name="image" type="url" onChange={e => setImage(e.target.value)} />
+                    {image && <img className="image-preview" src={image} alt="Choose" />}
+                </label>
+                <p>
+                    <button type="submit">Save</button>
+                    <button type="button">Cancel</button>
+                </p>
+            </Form>
         </div>
     );
+}
+
+export async function action({ request }) {
+    const formData = await request.formData();
+    const photo = Object.fromEntries(formData);
+
+    console.log("photo:", photo);
+
+    const photoObj = {
+        fields: {
+            caption: { stringValue: photo.caption },
+            image: { stringValue: photo.image },
+            uid: { stringValue: "IwlCsBmACaF4HOQCKdUB" }
+        }
+    };
+
+    const url = "https://firestore.googleapis.com/v1/projects/race-photo-app/databases/(default)/documents/photos";
+    const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(photoObj)
+    });
+
+    if (response.ok) {
+        return redirect("/photos");
+    }
 }
